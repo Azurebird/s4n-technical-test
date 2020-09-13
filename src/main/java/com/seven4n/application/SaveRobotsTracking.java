e05f4223f4471838dd11bdd4e567d6a55003ac1d
Azurebird <white.camilo@gmail.com>
2020-09-13 10:16:40 -0500

    Save utility class added

---
+++ src/main/java/com/seven4n/application/SaveRobotsTracking.java
@@ -0,0 +1,54 @@
+package com.seven4n.application;
+
+import com.seven4n.robot.CartesianPosition;
+import com.seven4n.robot.Robot;
+import com.seven4n.util.file.FileExternalSourceType;
+import com.seven4n.util.file.write.WriteExternalFile;
+import com.seven4n.util.file.write.WriteFileFactory;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Utility to save Robots results in files
+ */
+public final class SaveRobotsTracking {
+
+    // Private constructor to prevent unnecesary instances
+    private SaveRobotsTracking() {}
+
+    /**
+     * Saves a list of robot results into the destination path
+     * @param destinationPath The destination path to save the robots
+     * @param robotsToSave The robots to save
+     * @throws IOException In case the destination path is not a directory
+     */
+    public static void saveRobotsTracking(String destinationPath, List<Robot> robotsToSave) throws IOException {
+
+        final WriteExternalFile fileWritter = WriteFileFactory.getWriteExternalFile(FileExternalSourceType.LOCAL_FILE);
+
+        for (Robot robot: robotsToSave) {
+            List<String> linesToSave = new ArrayList<>();
+            linesToSave.add("== Reporte de entregas ==");
+            linesToSave.addAll(robot.getTracking().stream()
+                    .map(CartesianPosition::toString)
+                    .collect(Collectors.toList()));
+            fileWritter.writeFile(buildFilePath(destinationPath, robot.name), linesToSave);
+        }
+    }
+
+    /**
+     * Builds the file path for a specific robot concatenating the destination folder with the formatted robot name
+     * Ex:
+     * {destinationPath}/out{robotName}.txt
+     *
+     * @param destination The destination folder
+     * @param robotName The robot name
+     * @return A string with the absolute destination path
+     */
+    private static String buildFilePath(String destination, String robotName) {
+        return destination + "/out" + robotName + ".txt";
+    }
+}
---
+++ src/test/java/com/seven4n/application/SaveRobotsTrackingTest.java
@@ -0,0 +1,32 @@
+package com.seven4n.application;
+
+import com.seven4n.robot.Robot;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DisplayName("Test cases for the SaveRobotsTracking class")
+public class SaveRobotsTrackingTest {
+
+    @Test
+    @DisplayName("Should save the robots tracking into the defined folder")
+    public void testSaveRobotsTracking(@TempDir Path tempDir) throws IOException {
+        List<Robot> robots = LoadRobots.loadRobots("src/test/resources/robots/in");
+        robots.forEach(Robot::call);
+
+        SaveRobotsTracking.saveRobotsTracking(tempDir.toString(), robots);
+
+        File file = tempDir.toFile();
+
+        assertTrue(file.isDirectory());
+        assertEquals(2, file.listFiles().length);
+    }
+}
